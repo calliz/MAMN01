2575f27f40f5896ea8ca6174452faa78e8e51cda
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2012-11-23 00:12:43 +0100

    merged HapticGuide and StartRadar

--- handymap/src/com/example/map/StartActivityRadar.java
+++ handymap/src/com/example/map/StartActivityRadar.java
@@ -1,14 +1,24 @@
 package com.example.map;
 
+import org.haptimap.hcimodules.guiding.HapticGuide;
+import org.haptimap.hcimodules.guiding.HapticGuideEventListener;
+import org.haptimap.hcimodules.util.MyLocationModule;
+import org.haptimap.hcimodules.util.WayPoint;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,6 +26,12 @@ public class StartActivityRadar extends Activity implements SensorEventListener 
 	private ImageView imageView;
 	private SensorManager sensorManager;
 	private long lastUpdate;
+	private Button button1;
+	private Button button2;
+	private final String TAG = "MyActivity";
+	private MyLocationModule myLocation;
+	private Location currentPos;
+	private HapticGuide theGuide;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +40,80 @@ public class StartActivityRadar extends Activity implements SensorEventListener 
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		super.onCreate(savedInstanceState);
+
+		myLocation = new MyLocationModule(this);
+
+		myLocation.onStart();
+
+		currentPos = null;
+
+		theGuide = new HapticGuide(this);
+
 		setContentView(R.layout.main);
+
+		button1 = (Button) findViewById(R.id.set);
+
+		button1.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+
+				currentPos = myLocation.getCurrentLocation();
+				if (currentPos == null) {
+					Toast.makeText(StartActivityRadar.this,
+							"no GPS signal - no position set",
+							Toast.LENGTH_SHORT).show();
+
+				} else {
+					Toast.makeText(
+							StartActivityRadar.this,
+							"location set= " + currentPos.getLatitude() + ", "
+									+ currentPos.getLongitude(),
+							Toast.LENGTH_SHORT).show();
+				}
+				Log.i(TAG, "set button");
+
+			}
+
+		});
+
+		button2 = (Button) findViewById(R.id.guide);
+
+		button2.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+
+				if (currentPos != null) {
+
+					WayPoint goal = new WayPoint("goal", currentPos);
+
+					theGuide.setNextDestination(goal);
+
+					theGuide.onStart();
+				} else {
+					Toast.makeText(StartActivityRadar.this,
+							"no GPS signal - cannot guide", Toast.LENGTH_SHORT)
+							.show();
+				}
+				Log.i(TAG, "guide button");
+			}
+		});
+
+		theGuide.registerHapticGuideEventListener(new HapticGuideEventListener() {
+
+			public void onRateIntervalChanged(int millis) {
+
+			}
+
+			public void onPrepared(boolean onPrepared) {
+
+			}
+
+			public void onDestinationReached(long[] pattern) {
+				Toast.makeText(StartActivityRadar.this, "You have arrived!",
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+
 		imageView = (ImageView) findViewById(R.id.radar_image);
 		imageView.setImageResource(R.drawable.radar_2_black);
 
@@ -81,5 +170,14 @@ public class StartActivityRadar extends Activity implements SensorEventListener 
 					MapViewActivity.class);
 			StartActivityRadar.this.startActivity(myIntent);
 		}
+	}
+
+	@Override
+	protected void onDestroy() {
+
+		myLocation.onDestroy();
+		theGuide.onDestroy();
+
+		super.onDestroy();
 	}
 }
