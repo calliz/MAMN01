60e955aeb7f3866123393d5c031eb1c3fe75f892
Stefan Johansson <stefan.johansson@m.zaplox.com>
2012-11-26 16:27:41 +0100

    Added draw circles

---
+++ handymap/src/com/example/map/CircleOverlay.java
@@ -0,0 +1,52 @@
+package com.example.map;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+public class CircleOverlay extends Overlay {
+
+    Context context;
+    double mLat;
+    double mLon;
+    float mRadius;
+
+     public CircleOverlay(Context _context, double _lat, double _lon, float radius ) {
+            context = _context;
+            mLat = 55.70462000000001;//_lat;
+            mLon = 13.191360;//_lon;
+            mRadius = radius;
+     }
+
+     public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+
+         super.draw(canvas, mapView, shadow); 
+
+         Projection projection = mapView.getProjection();
+
+         Point pt = new Point();
+
+         GeoPoint geo = new GeoPoint((int) (mLat *1e6), (int)(mLon * 1e6));
+
+         projection.toPixels(geo ,pt);
+         float circleRadius = projection.metersToEquatorPixels(mRadius);
+
+         Paint innerCirclePaint;
+
+         innerCirclePaint = new Paint();
+         innerCirclePaint.setColor(Color.BLUE);
+         innerCirclePaint.setAlpha(25);
+         innerCirclePaint.setAntiAlias(true);
+
+         innerCirclePaint.setStyle(Paint.Style.FILL);
+
+         canvas.drawCircle((float)pt.x, (float)pt.y, circleRadius, innerCirclePaint);
+    }
+}
